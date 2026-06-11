6404a65732585816edc49e771097f1f075452607
Divyanshu3470 <divyanshunegi1435@gmail.com>
2026-06-11 07:58:42 +0530

    Remove unwanted file

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import userRoutes from "./Routes/userRoutes.js";
+import dotenv from 'dotenv'
 
 
 const app = express()
@@ -10,7 +11,9 @@ app.use(cors());
 app.use(express.json())
 app.use("/api", userRoutes);
 
-app.listen(8000 , (req, res)=>{
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT , (req, res)=>{
     console.log("server is running")
 })
 
